d840af0b407f607b53b520fb0f4f62d5a7718ed3
Adhithyas27 <120027992+Adhithyas27@users.noreply.github.com>
2023-01-12 09:31:07 +0530

    Add files via upload

---
+++ 2 staies test.py
@@ -0,0 +1,17 @@
+def fib(n):
+    if n <= 1:
+
+        return n
+
+    return fib(n-1) + fib(n-2)
+ 
+
+
+def countWays(s):
+
+    return fib(s + 1)
+ 
+s =int(input("Stairs"))
+print("Number of ways = ",countWays(s))
+
+ 
